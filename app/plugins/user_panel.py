e9829162f1b20b53e2abaea47e703afaae8f69f4
AliFreeGuy <alifreeguy@gmail.com>
2024-06-27 15:45:59 +0330

    feat : add show day for mounth

--- app/plugins/user_panel.py
+++ app/plugins/user_panel.py
@@ -3,28 +3,60 @@ from utils.utils import  alert , get_date_info , smtp_to_date
 from utils import text , btn 
 from utils import filters as f
 from utils import logger
-
+from utils import cache
 
 @Client.on_message(filters.private & f.user_is_join ,group=1)
 async def user_panel(client , message ):
     data  = get_date_info()
-    await client.send_message(chat_id = message.from_user.id , text = text.records_lists(data) , reply_markup = btn.parliran_lists_btn(data))
+
+    time = get_date_info()
+    record_keys = cache.redis.keys(f'recorder:*')
+    now_records = []
+
+    for record_date in record_keys:
+        now = time['now_date'].replace('/' , '-')
+        rdate = cache.redis.hget(record_date , 'date')[:-3].replace('/' , '-')
+        if now == rdate:
+                now_records.append(cache.redis.hget(record_date , 'date'))
+                
+    
+    now_records = sorted(list(set(now_records)))
+    await client.send_message(chat_id = message.from_user.id ,
+                               text = text.records_lists(data) ,
+                                 reply_markup = btn.parliran_lists_btn(data , now_records)
+                                 )
 
 
 
 @Client.on_callback_query( f.user_is_join , group=1)
 async def call_user_panel(client , call ):
     status = call.data.split(':')
+    print(call.data)
 
     if status[0] == 'date' : 
 
         time = get_date_info(int(status[1]))
+        record_keys = cache.redis.keys(f'recorder:*')
         
+        now_records = []
+
+        for record_date in record_keys:
+            now = time['now_date'].replace('/' , '-')
+            rdate = cache.redis.hget(record_date , 'date')[:-3].replace('/' , '-')
+            if now == rdate:
+                    now_records.append(cache.redis.hget(record_date , 'date'))
+                    
+        
+        now_records = sorted(list(set(now_records)))
+        
+        
+                
+
         try :
                     await client.edit_message_text(
                                                 chat_id = call.from_user.id ,
                                                 message_id = call.message.id ,
                                                 text = text.records_lists(time)  ,
-                                                reply_markup =btn.parliran_lists_btn(time) ,
+                                                reply_markup =btn.parliran_lists_btn(time , now_records) ,
                                                  )
         except Exception as e :print(e)
--- app/utils/btn.py
+++ app/utils/btn.py
@@ -11,9 +11,20 @@ def join_channels_url(channels):
     return InlineKeyboardMarkup(buttons)
 
 
-def parliran_lists_btn(data):
+def parliran_lists_btn(data , now_records):
     
     buttons = []
+    
+
+    for i in range(0, len(now_records), 3):
+        row = []
+        row.append(InlineKeyboardButton(text=now_records[i], callback_data=f'get_record:{now_records[i]}'))
+        if i + 1 < len(now_records):
+            row.append(InlineKeyboardButton(text=now_records[i + 1], callback_data=f'get_record:{now_records[i + 1]}'))
+        if i + 2 < len(now_records):
+            row.append(InlineKeyboardButton(text=now_records[i + 2], callback_data=f'get_record:{now_records[i + 2]}'))
+        buttons.append(row)
+
     buttons.append([
         InlineKeyboardButton(text='◀️',callback_data=f'date:{data["before_smtp"]}'),
         InlineKeyboardButton(text=data['now_date'],callback_data=f'date:{data["now_smtp"]}'),
